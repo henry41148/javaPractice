f6cb5507ace633cd9d155722f8503e18c6df4895
henry41148 <henry.41148@gmail.com>
2023-10-13 20:23:01 +0700

    lab4 submit

---
+++ Lab4/src/main/java/vn/edu/tdtu/lab4/model/Product.java
@@ -0,0 +1,22 @@
+package vn.edu.tdtu.lab4.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @Setter @Getter
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+
+    public Product(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
